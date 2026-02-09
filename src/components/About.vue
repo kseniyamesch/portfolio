3a8183db71f3c3169498634735596977b102fafa
Kseniia <kseniyamesch0101@gmail.com>
2026-02-09 19:39:16 +0100

    about section added

--- src/components/About.vue
+++ src/components/About.vue
@@ -1,42 +1,45 @@
 <template>
-  <section id="about" class="px-6 py-4">
-    <h2>About me</h2>
-    <p>I am a junior developer with a background in front-end and growing experience in .NET development.
+  <section id="about" class="px-8 py-8 bg-[rgba(184,156,125,0.2)]">
+    <h2 class="text-primaryText bold text-2xl uppercase mb-6!">About me</h2>
+    <p class="italic">I am a junior developer with a background in front-end and growing experience in .NET development.
       I enjoy building clear and user-friendly applications and learning how things work under the hood.
       I am curious, detail-oriented and motivated to keep improving my skills through real projects.</p>
-      <p>I have completed training and small internship projects focused on frontend and backend development, which helped me understand how full applications are built.</p>
-    <h3>Frontend (main focus)</h3>
+      <br>
+      <p class="italic">I have completed training and small internship projects focused on frontend and backend development, which helped me understand how full applications are built.</p>
+    <h3 class="mt-6! mb-2! text-lg font-semibold text-primaryText uppercase">Frontend (main focus)</h3>
     <ul class="flex flex-wrap gap-2">
-      <li class="px-3 py-1 bg-gray-200 rounded-full text-sm">HTML5</li>
-      <li class="px-3 py-1 bg-gray-200 rounded-full text-sm">CSS3</li>
-      <li class="px-3 py-1 bg-gray-200 rounded-full text-sm">JavaScript (ES6+)</li>
-      <li class="px-3 py-1 bg-gray-200 rounded-full text-sm">Vue.js</li>
-      <li class="px-3 py-1 bg-gray-200 rounded-full text-sm">basic React</li>
+      <li class="px-3 py-1 bg-white border-2 border-accent rounded-full text-sm text-gray-800">HTML5</li>
+      <li class="px-3 py-1 bg-white border-2 border-accent rounded-full text-sm text-gray-800">CSS3</li>
+      <li class="px-3 py-1 bg-white border-2 border-accent rounded-full text-sm text-gray-800">JavaScript (ES6+)</li>
+      <li class="px-3 py-1 bg-white border-2 border-accent rounded-full text-sm text-gray-800">Vue.js</li>
+      <li class="px-3 py-1 bg-white border-2 border-accent rounded-full text-sm text-gray-800">basic React</li>
     </ul>
     
-    <h3>Backend (main focus)</h3>
-    <ul class="flex flex-wrap gap-2">
-      <li class="px-3 py-1 bg-gray-200 rounded-full text-sm">C#</li>
-      <li class="px-3 py-1 bg-gray-200 rounded-full text-sm">.NET Core</li>
-      <li class="px-3 py-1 bg-gray-200 rounded-full text-sm">ASP.NET MVC</li>
-      <li class="px-3 py-1 bg-gray-200 rounded-full text-sm">Web APIs</li>
-      <li class="px-3 py-1 bg-gray-200 rounded-full text-sm">Entity Framework</li>
+    <h3 class="mt-6! mb-2! text-lg font-semibold text-primaryText">Backend (main focus)</h3>
+    <ul class="flex flex-wrap gap-2 uppercase"> 
+      <li class="px-3 py-1 bg-white border-2 border-accent rounded-full text-sm text-gray-800">C#</li>
+      <li class="px-3 py-1 bg-white border-2 border-accent rounded-full text-sm text-gray-800">.NET Core</li>
+      <li class="px-3 py-1 bg-white border-2 border-accent rounded-full text-sm text-gray-800">ASP.NET MVC</li>
+      <li class="px-3 py-1 bg-white border-2 border-accent rounded-full text-sm text-gray-800">Web APIs</li>
+      <li class="px-3 py-1 bg-white border-2 border-accent rounded-full text-sm text-gray-800">Entity Framework</li>
     </ul>
   
-    <h3>Also familiar with:</h3>
+    <h3 class="mt-6! mb-2! text-lg font-semibold text-primaryText uppercase">Also familiar with:</h3>
     <ul class="flex flex-wrap gap-2">
-      <li class="px-3 py-1 bg-gray-200 rounded-full text-sm">Angular</li>
-      <li class="px-3 py-1 bg-gray-200 rounded-full text-sm">Node.js</li>
-      <li class="px-3 py-1 bg-gray-200 rounded-full text-sm">MongoDB</li>
-      <li class="px-3 py-1 bg-gray-200 rounded-full text-sm">MySQL</li>
-      <li class="px-3 py-1 bg-gray-200 rounded-full text-sm">Bootstrap</li>
-      <li class="px-3 py-1 bg-gray-200 rounded-full text-sm">Tailwind CSS</li>
-      <li class="px-3 py-1 bg-gray-200 rounded-full text-sm">Git</li>
+      <li class="px-3 py-1 bg-white border-2 border-accent rounded-full text-sm text-gray-800">Angular</li>
+      <li class="px-3 py-1 bg-white border-2 border-accent rounded-full text-sm text-gray-800">Node.js</li>
+      <li class="px-3 py-1 bg-white border-2 border-accent rounded-full text-sm text-gray-800">MongoDB</li>
+      <li class="px-3 py-1 bg-white border-2 border-accent rounded-full text-sm text-gray-800">MySQL</li>
+      <li class="px-3 py-1 bg-white border-2 border-accent rounded-full text-sm text-gray-800">Bootstrap</li>
+      <li class="px-3 py-1 bg-white border-2 border-accent rounded-full text-sm text-gray-800">Tailwind CSS</li>
+      <li class="px-3 py-1 bg-white border-2 border-accent rounded-full text-sm text-gray-800">Git</li>
     </ul>
+    <a href="#projects" class="inline-block mt-9! px-6 py-2 bg-accent text-white uppercase rounded-md font-semibold hover:bg-accent-dark hover:scale-105 transition duration-200">See projects</a>
+    
   </section>
 </template>
 
-<style>
+<style scoped>
 /* @media (min-width: 1024px) {
   .about {
     min-height: 100vh;
@@ -44,4 +47,11 @@
     align-items: center;
   }
 } */
+
+section {
+  /* border-top: 4px solid rgba(59, 47, 47, 0.4); */
+  /* background-color: rgba(184, 156, 125, 0.2); */
+  border-radius: 5px;
+}
+
 </style>
