e4fb2fea0e0dcc84b5d363a4afd769b9156d1e9a
Kseniia <kseniyamesch0101@gmail.com>
2026-02-23 18:19:55 +0100

    contacts section added

--- src/components/Contacts.vue
+++ src/components/Contacts.vue
@@ -1,6 +1,16 @@
 <template>
-    <section id="contacts" class="px-6 py-4">
-        Contacts page
+    <section id="contacts" class="px-6 py-4 bg-[rgba(184,156,125,0.2)]">
+        <div class="w-[500px] bg-accent text-white mx-auto! px-6 py-4 rounded">
+            <h3 class="uppercase bold text-2xl mb-4!">Contact me:</h3>
+            <ul>
+                <li><a href="tel:+32476029813"><span></span>0476029813</a></li>
+                <li><a href="mailto:kseniyamesch0101@gmail.com">kseniyamesch0101@gmail.com</a></li>
+                <li><a href="https://github.com/kseniyamesch" rel="noopener noreferrer" target="_blank"></a>GitHub</li>
+                <li><a href=""></a></li>
+            </ul>
+            <p>GitHub</p>
+            <p>LinkedIn</p>
+        </div>
     </section>
 </template>
 
@@ -8,4 +18,6 @@
 
 </script>
 
-<style scoped></style>
+<style scoped>
+
+</style>
